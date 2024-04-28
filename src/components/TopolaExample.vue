169b019c332519e9aace4944b506456aae8b1c17
Ed Tewiah <etewiah@hotmail.com>
2024-04-28 09:32:45 +0100

    Add simple TopolaExample component

---
+++ src/components/TopolaExample.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div id="topola-chart"></div>
+  </div>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'TopolaExample'
+})
+
+import * as topola from 'topola';
+import { ref, onMounted } from 'vue'
+
+const data = {
+  indis: [
+    {
+      id: 'I1',
+      firstName: 'John',
+      lastName: 'Smith',
+      famc: 'F1',
+    },
+    {
+      id: 'I2',
+      firstName: 'Peter',
+      lastName: 'Smith',
+      fams: ['F1'],
+    },
+    {
+      id: 'I3',
+      firstName: 'Laura',
+      lastName: 'Abbot',
+      fams: ['F1'],
+    },
+  ],
+  fams: [
+    {
+      id: 'F1',
+      husb: 'I2',
+      wife: 'I3',
+      children: ['I1'],
+    },
+  ],
+};
+
+onMounted(() => {
+  const chart = topola.createChart({
+    json: data,
+    chartType: topola.HourglassChart,
+    renderer: topola.DetailedRenderer,
+  });
+  chart.render(document.getElementById('topola-chart'));
+});
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,14 +1,14 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <TopolaExample />
+    <!-- <img alt="Quasar logo"
+         src="~assets/quasar-logo-vertical.svg"
+         style="width: 200px; height: 200px"> -->
   </q-page>
 </template>
 
 <script setup>
+import TopolaExample from 'components/TopolaExample.vue'
 defineOptions({
   name: 'IndexPage'
 });
