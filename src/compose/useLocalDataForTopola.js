d784713d8b85534eddd24cb9cf270a37495f51f0
Ed Tewiah <etewiah@hotmail.com>
2024-04-30 14:44:05 +0100

    Add prettierrc and use to format js file

--- src/compose/useLocalDataForTopola.js
+++ src/compose/useLocalDataForTopola.js
@@ -1,23 +1,23 @@
-import { LocalStorage } from "quasar";
+import { LocalStorage } from 'quasar'
 export default function () {
   function updateLocalConfig(confKey, confValue) {
-    let localConf = getLocalTopolaConfig() || {};
-    localConf[confKey] = confValue;
-    LocalStorage.set("topola-config", localConf);
-    return "";
+    let localConf = getLocalTopolaConfig() || {}
+    localConf[confKey] = confValue
+    LocalStorage.set('topola-config', localConf)
+    return ''
   }
   function getLocalTopolaConfig() {
-    let topolaConfig = LocalStorage.getItem("topola-config");
+    let topolaConfig = LocalStorage.getItem('topola-config')
     return (
       topolaConfig || {
-        topolaChartType: "HourglassChart",
-        topolaRenderer: "DetailedRenderer",
+        topolaChartType: 'HourglassChart',
+        topolaRenderer: 'DetailedRenderer',
       }
-    );
+    )
   }
 
   return {
     getLocalTopolaConfig,
     updateLocalConfig,
-  };
+  }
 }
