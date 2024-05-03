c333d07dff4baa28564941cefc8f50209b2e207e
Ed Tewiah <etewiah@hotmail.com>
2024-05-03 21:46:30 +0100

    More info about individual

--- src/components/TopolaIndividual.vue
+++ src/components/TopolaIndividual.vue
@@ -1,7 +1,9 @@
 <template>
   <div v-if="currentPerson">
     <div>Focus on</div>
-    <div class="text-subtitle1"> {{ currentPerson.firstName }} {{ currentPerson.lastName }}</div>
+    <div class="text-h6"> {{ fullPersonName }}</div>
+    <div class="text-subtitle1"> {{ birthInformation }}</div>
+    <div class="text-subtitle1"> {{ notesAboutPerson }}</div>
     <!-- <div>{{ currentPerson }}</div> -->
   </div>
 </template>
@@ -20,6 +22,35 @@ export default {
   data: () => ({
     currentPerson: null
   }),
+  computed: {
+    birthInformation() {
+      if (this.currentPerson.birth.place) {
+        let birthInformation = `${this.currentPerson.firstName} ${this.currentPerson.lastName} was born in ${this.currentPerson.birth.place}`
+        if (this.currentPerson.death.place) {
+          birthInformation = `${birthInformation} and died in ${this.currentPerson.death.place}`
+        }
+        return birthInformation
+      } else {
+        return ""
+      }
+    },
+    notesAboutPerson() {
+      return ""
+      // Disabling for now
+      // if (this.currentPerson.notes) {
+      //   let notesAboutPerson = ""
+      //   this.currentPerson.notes.forEach(personNote => {
+      //     notesAboutPerson = `${notesAboutPerson} ${personNote}`
+      //   });
+      //   return notesAboutPerson
+      // } else {
+      //   return ""
+      // }
+    },
+    fullPersonName() {
+      return `${this.currentPerson.firstName} ${this.currentPerson.lastName}`
+    }
+  },
   methods: {
     updateCurrentUser() {
       let currentPersonID = this.$route.query.personID
