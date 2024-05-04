b221e5d344a0070b12f5f2cf3b21a8e13e5bb377
Ed Tewiah <etewiah@hotmail.com>
2024-05-04 19:55:48 +0100

    Refactor useTopolaData to make it more conventional

--- src/compose/useTopolaData.js
+++ src/compose/useTopolaData.js
@@ -1,81 +1,72 @@
 // useTopolaData.js
 
-// import { ref } from 'vue'
-import * as topola from 'topola'
-import { useRoute } from 'vue-router'
+// import * as topola from 'topola'
+// import { useRoute } from 'vue-router'
 
-export default function useTopolaData(allJsonData) {
-  const route = useRoute()
+export default function () {
+  function getFocusedData(allJsonData, focusedIndiForGraph) {
+    // const route = useRoute()
 
-  // Function to find the intersection of two arrays
-  function intersection(arr1, arr2) {
-    const set1 = new Set(arr1)
-    const set2 = new Set(arr2)
-    const intersectionSet = new Set([...set1].filter((num) => set2.has(num)))
-    return [...intersectionSet]
-  }
-
-  // Find the current person's ID
-  let currentPersonID = route.query.personID
-  let focusedIndiForGraph = allJsonData.indis.find(
-    (individual) => individual.id === currentPersonID
-  )
-
-  // If current person is not found, use the first individual in the data
-  if (!focusedIndiForGraph) {
-    focusedIndiForGraph = allJsonData.indis[0]
-    currentPersonID = focusedIndiForGraph.id
-  }
-
-  // Filter families to include only those where the individual is mentioned
-  const relatedFamilies = allJsonData.fams.filter((family) => {
-    return (
-      family.husb === currentPersonID ||
-      family.wife === currentPersonID ||
-      family.children.includes(currentPersonID)
-    )
-  })
-
-  // Function to filter out family IDs from an individual's associations
-  function stripFamIdsFromIndi(famIdsToKeep, Indi) {
-    if (!famIdsToKeep.includes(Indi.famc)) {
-      Indi.famc = null
+    // Function to find the intersection of two arrays
+    function intersection(arr1, arr2) {
+      const set1 = new Set(arr1)
+      const set2 = new Set(arr2)
+      const intersectionSet = new Set([...set1].filter((num) => set2.has(num)))
+      return [...intersectionSet]
     }
-    Indi.fams = intersection(Indi.fams, famIdsToKeep)
-    return Indi
+
+    // Filter families to include only those where the individual is mentioned
+    const relatedFamilies = allJsonData.fams.filter((family) => {
+      return (
+        family.husb === focusedIndiForGraph.id ||
+        family.wife === focusedIndiForGraph.id ||
+        family.children.includes(focusedIndiForGraph.id)
+      )
+    })
+
+    // Function to filter out family IDs from an individual's associations
+    function stripFamIdsFromIndi(famIdsToKeep, Indi) {
+      if (!famIdsToKeep.includes(Indi.famc)) {
+        Indi.famc = null
+      }
+      Indi.fams = intersection(Indi.fams, famIdsToKeep)
+      return Indi
+    }
+
+    // Extract related family IDs
+    let relatedFamilyIds = relatedFamilies.map((family) => family.id)
+
+    // Extract individual IDs of all related individuals
+    let focusedIndividualIds = []
+    relatedFamilies.forEach((family) => {
+      let currFamilyMemberIds = [...family.children, family.husb, family.wife]
+      let mergedSet = new Set([...focusedIndividualIds, ...currFamilyMemberIds])
+      focusedIndividualIds = [...mergedSet]
+    })
+
+    // Filter individuals based on related individual IDs
+    let focusedIndividuals = allJsonData.indis.filter((individual) =>
+      focusedIndividualIds.includes(individual.id)
+    )
+
+    // Clean individual data
+    let cleanedRelatedIndivs = []
+    focusedIndividuals.forEach((indiv) => {
+      let cleandIndiv = stripFamIdsFromIndi(relatedFamilyIds, indiv)
+      cleanedRelatedIndivs.push(cleandIndiv)
+    })
+
+    // Prepare JSON data
+    const topolaJsonData = {
+      indis: cleanedRelatedIndivs,
+      fams: relatedFamilies,
+    }
+
+    return {
+      topolaJsonData,
+    }
   }
-
-  // Extract related family IDs
-  let relatedFamilyIds = relatedFamilies.map((family) => family.id)
-
-  // Extract individual IDs of all related individuals
-  let focusedIndividualIds = []
-  relatedFamilies.forEach((family) => {
-    let currFamilyMemberIds = [...family.children, family.husb, family.wife]
-    let mergedSet = new Set([...focusedIndividualIds, ...currFamilyMemberIds])
-    focusedIndividualIds = [...mergedSet]
-  })
-
-  // Filter individuals based on related individual IDs
-  let focusedIndividuals = allJsonData.indis.filter((individual) =>
-    focusedIndividualIds.includes(individual.id)
-  )
-
-  // Clean individual data
-  let cleanedRelatedIndivs = []
-  focusedIndividuals.forEach((indiv) => {
-    let cleandIndiv = stripFamIdsFromIndi(relatedFamilyIds, indiv)
-    cleanedRelatedIndivs.push(cleandIndiv)
-  })
-
-  // Prepare JSON data
-  const topolaJsonData = {
-    indis: cleanedRelatedIndivs,
-    fams: relatedFamilies,
-  }
-
   return {
-    topolaJsonData,
-    focusedIndiForGraph,
+    getFocusedData,
   }
 }
--- src/pages/TopolaStaticDataPage.vue
+++ src/pages/TopolaStaticDataPage.vue
@@ -23,7 +23,7 @@ import TopolaIndividual from 'components/TopolaIndividual.vue'
 import topolaGedcomData from 'src/data/private/Campbells-Dowdalls-Borthwicks.ged.js';
 import * as topola from 'topola';
 // import { parse as parseGedcom, toD3Force, toDot } from 'parse-gedcom';
-// import { useRouter, useRoute } from 'vue-router'
+import { useRoute } from 'vue-router'
 import useTopolaData from 'src/compose/useTopolaData';
 
 defineOptions({
@@ -38,6 +38,20 @@ const props = defineProps({
 
 // Convert GEDCOM data to JSON format
 const allJsonData = topola.gedcomToJson(topolaGedcomData)
-const { topolaJsonData, focusedIndiForGraph } = useTopolaData(allJsonData);
+
+const { getFocusedData } = useTopolaData()
+
+// Find the current person's ID
+const route = useRoute()
+let currentPersonID = route.query.personID
+let focusedIndiForGraph = allJsonData.indis.find(
+  (individual) => individual.id === currentPersonID
+)
+// If current person is not found, use the first individual in the data
+if (!focusedIndiForGraph) {
+  focusedIndiForGraph = allJsonData.indis[0]
+}
+
+const { topolaJsonData } = getFocusedData(allJsonData, focusedIndiForGraph);
 
 </script>
